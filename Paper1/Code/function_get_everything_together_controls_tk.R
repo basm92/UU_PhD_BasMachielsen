e2ce5ec5b9be5de1e481ca01b178e4fc00a1df9c
basm92 <basmachielsen@live.nl>
2020-07-06 11:02:23 +0200

    write get id fucntion

--- Paper1/Code/function_get_everything_together_controls_tk.R
+++ Paper1/Code/function_get_everything_together_controls_tk.R
@@ -16,7 +16,33 @@ library(stringr)
 
 # find_econcontrols <- nog maken
 
+#find_politician_id
 
+
+find_politician_id <- function(names, date) {
+  politicians <- readxl::read_excel("./Data/tk_1815tot1950uu.xlsx", sheet = 1)
+  
+  temp <- politicians %>%
+    janitor::clean_names() %>%
+    mutate(begin_periode = ymd(begin_periode), 
+          einde_periode = ymd(einde_periode)) %>%
+    filter(date > begin_periode, date < einde_periode)
+  
+  matches <- stringdist::amatch(names, temp$achternaam, method = "jw", maxDist = 10)
+  
+  data.frame(names = names, 
+             polnames = temp$achternaam[matches],
+             polid = temp$b1_nummer[matches]
+             )
+}
+
+##Example
+date <- ymd("1880-01-01")
+names <- c("Godefroi","Idzerda", "Bieberstein")
+
+find_politician_id(names, date)
+
+#find_district
 #From the politician number, find the district
 politicians <- read_excel("./Data/tk_1815tot1950uu.xlsx", sheet = 1)
 career <- read_excel("./Data/tk_1815tot1950uu.xlsx", sheet = 2)
